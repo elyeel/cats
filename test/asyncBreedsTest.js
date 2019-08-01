35a767fba02b05181277536ec20470d603a43107
elyeel <elyeel@yahoo.com>
2019-08-01 00:46:35 +0000

    added 5th tests and saphire text

--- test/asyncBreedsTest.js
+++ test/asyncBreedsTest.js
@@ -3,7 +3,7 @@ const assert = require('chai').assert;
 const breedDetailsFromFile = require('../asyncBreeds');
 
 describe('#breedDetailsFromFile', () => {
-  it('provides, via callback, breed details for the Bombay breed', (done) => {
+  it('provides, via callback, breed details for the Balinese breed', (done) => {
     breedDetailsFromFile('Balinese', (bombay) => {
       const expectedDesc = "Balinese are curious, outgoing, intelligent cats with excellent communication skills. They are known for their chatty personalities and are always eager to tell you their views on life, love, and what you’ve served them for dinner.";
       assert.equal(expectedDesc, bombay);
@@ -19,4 +19,12 @@ describe('#breedDetailsFromFile', () => {
       done(); 
     });      
   });
+
+  it('provides, via callback, undefined for a breed that does not exist', (done) => {
+    breedDetailsFromFile('Saphire', (desc) => {
+      
+      assert.equal('', desc);
+      done(); 
+    })
+  }); 
 });
